5d421efcfe45da24275507a865e3cdfa6fa4fa0f
Hari Charan Karanam <HariCharanKaranam@Haris-MacBook-Air.local>
2018-01-22 22:01:17 -0500

    print

---
+++ prog3_print_function.py
@@ -0,0 +1,25 @@
+#! /usr/bin/pyton
+
+highScoreValue = 1      #This is camel case style variable
+a = 1
+A = 2
+Red_007_hat = 100
+_007 = 200
+_007_PyCharm = 200
+
+print("Let's access camel case style variable ", highScoreValue)
+print(a, "A lower case variable")
+print("Access an upper case ", A, " variable")
+print("Combination : ", Red_007_hat)
+print(_007)
+print('Another combo : ', _007_PyCharm)
+
+UserInfo = "Harry"
+Language = 'Python'
+_Version = 3.6
+Use = "App Development"
+
+
+print('User info : {} and Language : {}' .format({}, {}))
+print("")
+print('Version is {} '.format({3}))
