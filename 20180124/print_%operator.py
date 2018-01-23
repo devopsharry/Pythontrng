0d609514a80f2fc276fe357e95906437974e9812
Hari Charan Karanam <HariCharanKaranam@Haris-MacBook-Air.local>
2018-01-23 15:30:58 -0500

    Printexamples

---
+++ 20180124/print_%operator.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+# Example 4 - using % operator
+
+firstName='Venkata'
+middleName='Vishruth'
+_LastName='Julakanti'
+_school='Allamanda'
+_Version = 3.6
+
+print('Hello, First Name -  %s %s, Last Name - %s' %(firstName,middleName,_LastName))
+print("")
+print("School Name is - %s and Version is %d" %(_school,_Version))
+print('')
+print("Testing the float display -  Version is %f" %(_Version))
+print()
+print("Location : %s & Date : %d" %('India',20180123))
---
+++ 20180124/print_formatmethod.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+Used_for = "App Development"
+Version = 3.6
+
+print(Used_for, id(Used_for), type(Used_for))
+print("")
+print(Version, id(Version), type(Version))
+print("")
+print('Python is used for {} and course is {}'.format(Used_for, Version))
+
+#Example 2
+Phone='iPhone7'
+Laptop="Macbook Pro"
+print("")
+print('I like {0} and {1}'.format(Phone, Laptop))
+print('')
+
+#Example 3
+print("Hello {Name}, {Greeting}".format(Greeting='Good Morning', Name='Harry'))
